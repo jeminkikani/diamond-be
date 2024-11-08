bad2499083398a2e469c9d5c3795a711cbe5a973
skilljeminkikani <skilljeminkikani9510@gmail.com>
2024-11-08 20:37:37 +0530

    login chnages

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const brokerRouter = require("./broker.routes");
 const diamondRouter = require("./diamond.routes");
-const loginRouter = require("./login.Routes");
+const loginRouter = require("./login.routes");
 const { verifytoken } = require("../middleware/auth");
 
 const router = express.Router();
